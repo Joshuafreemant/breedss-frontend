09586dd9a09143ae2863fbb65b0ffca164548fd3
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2022-12-06 22:14:18 +0100

    Update Ads.jsx

--- src/Components/Ads.jsx
+++ src/Components/Ads.jsx
@@ -4,19 +4,19 @@ import googleAds from '../Images/googleAds.png'
 const Ads = ({slot}) => {
   // const { currentPath } = props
   useEffect(() => {
-    window.adsbygoogle = window.adsbygoogle || []
-    window.adsbygoogle.push({})
+    // window.adsbygoogle = window.adsbygoogle || []
+    // window.adsbygoogle.push({})
   }, [])
   return (
     <div className='ads-container w-[300px] overflow-hidden hidden lg:flex'>
-      <ins class="adsbygoogle"
+      {/* <ins class="adsbygoogle"
         style={{ width: "100%" }}
         data-ad-format="fluid"
         // data-ad-layout-key="-Yo-ur-la-yo-ut-ke-y"
         data-ad-client="ca-pub-8217530327568975"
         data-ad-slot={slot}
         data-full-width-responsive="true">
-      </ins>
+      </ins> */}
 
     </div>
   )
